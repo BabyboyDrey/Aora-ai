a2d5f223885fc4eeb16d4932f0a50b8170ab4f16
Dare Babs <darebabs@Macs-MacBook-Pro-2.local>
2024-08-20 13:56:48 +0100

    pop

--- utils/passport.js
+++ utils/passport.js
@@ -1,11 +1,8 @@
 const passport = require("passport");
 const FacebookStrategy = require("passport-facebook").Strategy;
 const expressSession = require("express-session");
-const Users = require("../models/users"); // Assuming your User model is here
+const Users = require("../models/users");
 require("dotenv").config();
-
-const ID = "1048252620278389";
-const SECRET = "3ab164c1912a7cac2628b818b1b53d0c";
 
 passport.use(
   new FacebookStrategy(
@@ -18,26 +15,21 @@ passport.use(
     async (accessToken, refreshToken, profile, done) => {
       try {
         console.log("Facebook profile:", profile);
-        // Extract user details from profile
         const { id, emails, name } = profile;
         const email = emails && emails.length > 0 ? emails[0].value : null;
 
-        // Check if user already exists
         let user = await Users.findOne({ facebookId: id });
 
         if (!user && email) {
-          // If user doesn't exist, check if there's a user with the same email
           user = await Users.findOne({ email_address: email });
 
           if (!user) {
-            // If the user doesn't exist, create a new user
             user = await Users.create({
               facebookId: id,
               email_address: email,
               full_name: `${name.givenName} ${name.familyName}`,
             });
           } else {
-            // Link the existing user to their Facebook account
             user.facebookId = id;
             await user.save();
           }
@@ -52,7 +44,7 @@ passport.use(
 );
 
 passport.serializeUser((user, callback) => {
-  callback(null, user.id); // Store only the user ID in the session
+  callback(null, user.id);
 });
 
 passport.deserializeUser(async (id, callback) => {

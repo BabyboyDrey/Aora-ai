eaeec4d0640af5dbb6b7ecce4b15d7eb04d501df
Dare Babs <darebabs@Macs-MacBook-Pro-2.local>
2024-09-01 18:33:13 +0100

    llp

--- controllers/userDesignBook/designBook.js
+++ controllers/userDesignBook/designBook.js
@@ -11,8 +11,8 @@ router.post(
       const items = req.body;
       const formatDate = (date) => {
         const year = date.getFullYear();
-        const month = String(date.getMonth() + 1).padStart(2, "0"); // Months are zero-indexed
-        const day = String(date.getDate()).padStart(2, "0"); // Day of the month
+        const month = String(date.getMonth() + 1).padStart(2, "0");
+        const day = String(date.getDate()).padStart(2, "0");
 
         return `${year}${month}${day}`;
       };

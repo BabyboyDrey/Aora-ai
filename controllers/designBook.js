73fd7ed63bb6dd0cb853c14a80b694df4cf06234
Babalola Dare <babaloladare921@gmail.com>
2024-10-09 09:35:14 +0100

    ss;

--- controllers/designBook.js
+++ controllers/designBook.js
@@ -1,7 +1,12 @@
 const router = require("express").Router();
 const asyncErrCatcher = require("../middlewares/asyncErrCatcher");
 const userAuth = require("../middlewares/userAuth");
+const clothings = require("../models/clothings");
 const DesignBook = require("../models/designBook");
+const fabric = require("../models/fabric");
+const models = require("../models/models");
+const styles = require("../models/styles");
+const textContent = require("../models/textContent");
 
 router.post(
   "/create-design-book",
@@ -72,6 +77,75 @@ router.get(
     } catch (err) {
       console.error(err);
       next(err);
+    }
+  })
+);
+
+router.get(
+  "/get-all-design-book-contents/:designBookId",
+  userAuth,
+  asyncErrCatcher(async (req, res, next) => {
+    try {
+      const { designBookId } = req.params;
+      if (!designBookId) {
+        throw new Error("Required parameter not provided!");
+      }
+      const foundDesignBook = await DesignBook.findOne({
+        userId: req.user.id,
+        _id: designBookId,
+      });
+
+      if (!foundDesignBook) {
+        throw new Error("Design book not found!");
+      }
+
+      const foundClothes = await clothings.find({
+        designBookId: designBookId,
+        userId: req.user.id,
+      });
+      const foundFabrics = await fabric.find({
+        designBookId: designBookId,
+        userId: req.user.id,
+      });
+      const foundModels = await models.find({
+        designBookId: designBookId,
+        userId: req.user.id,
+      });
+      const foundStyles = await styles.find({
+        designBookId: designBookId,
+        userId: req.user.id,
+      });
+      const foundTextContent = await textContent.find({
+        designBookId: designBookId,
+        userId: req.user.id,
+      });
+
+      if (
+        foundClothes.length === 0 &&
+        foundFabrics.length === 0 &&
+        foundModels.length === 0 &&
+        foundStyles.length === 0 &&
+        foundTextContent.length === 0
+      ) {
+        return res.status(200).json({
+          success: false,
+          message: "No data created",
+        });
+      }
+
+      res.status(200).json({
+        success: true,
+        data: {
+          foundClothes,
+          foundFabrics,
+          foundModels,
+          foundStyles,
+          foundTextContent,
+        },
+      });
+    } catch (err) {
+      console.error(err);
+      next(err.message);
     }
   })
 );

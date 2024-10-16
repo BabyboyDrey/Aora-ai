b47ba7285f5fdf1ce659edc61ae63d087b37ad95
Babalola Dare <babaloladare921@gmail.com>
2024-10-16 22:06:17 +0100

    cleaned code update

--- backend/utils/checkAndDeleteFile.js
+++ backend/utils/checkAndDeleteFile.js
@@ -1,33 +1,17 @@
 const fs = require("fs");
 
-// function checkAndDeleteFile(filePath, callback) {
-//   fs.access(filePath, fs.constants.F_OK, (err) => {
-//     if (err) {
-//       return callback(null);
-//     }
-
-//     fs.unlink(filePath, (unlinkErr) => {
-//       if (unlinkErr) {
-//         return callback(unlinkErr);
-//       }
-//       return callback(null);
-//     });
-//   });
-// }
-
 function checkAndDeleteFile(filePath) {
   return new Promise((resolve, reject) => {
     fs.access(filePath, fs.constants.F_OK, (err) => {
       if (err) {
-        // If the file does not exist, resolve the promise without deleting
         return resolve();
       }
 
       fs.unlink(filePath, (unlinkErr) => {
         if (unlinkErr) {
-          return reject(unlinkErr); // Reject if there was an error deleting the file
+          return reject(unlinkErr);
         }
-        return resolve(); // Resolve if the file was deleted successfully
+        return resolve();
       });
     });
   });

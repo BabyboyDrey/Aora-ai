14299e7bda6117e64d2cb43d52d9d3f94e7f1281
Babalola Dare <babaloladare921@gmail.com>
2024-10-25 13:22:49 +0100

    f

--- controllers/designService.js
+++ controllers/designService.js
@@ -13,7 +13,10 @@ router.get(
     try {
       const foundDesignServices = await designService.find({});
       if (foundDesignServices.length === 0) {
-        throw new Error("No design service created!");
+        return res.status(404).json({
+          error: true,
+          message: "No design service created!",
+        });
       }
       res.json({
         success: true,
